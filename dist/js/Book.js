1daa284a758b3e30267eb4871dd9a11cfd12c783
iamndlovu <ndlovu.p@protonmail.com>
2020-08-15 20:39:49 +0200

    class for Book object

---
+++ dist/js/Book.js
@@ -0,0 +1,12 @@
+class Book {
+  constructor(title, language, hash, summary) {
+    this.title = title;
+    this.hash = hash;
+    this.language = language;
+    this.summary = summary
+  }
+
+  build() {
+    
+  }
+}
