519838c16af21bb09e08e8a08a4d017d2758053a
iamndlovu <ndlovu.p@protonmail.com>
2020-08-15 20:39:02 +0200

    toggle book display

---
+++ dist/js/books.js
@@ -0,0 +1,33 @@
+const categoryTitle = document.querySelectorAll('.category-title'),
+      dropdownIcon = document.querySelectorAll('.dropdown-icon');
+
+dropdownIcon.forEach(icon => {icon.addEventListener('click', toggleBooks)});
+categoryTitle.forEach(title => title.addEventListener('click', toggleBooks));
+
+//show or hide books
+function toggleBooks(e) {
+  const target = e.target;
+  let language;
+
+  //get language whose books we want to toggle
+  if ((target.classList.contains('category-title')) || (target.classList.contains('dropdown-icon'))) {
+    language = target.parentElement.id;
+
+  } else if (target.classList.contains('dropdown-line')) {
+    language = target.parentElement.parentElement.id;
+  } else {
+    return;
+  }
+
+  //select container and dropdown icon to toggle
+  const booksContainer = document.querySelector(`#${language.toLowerCase()}`),
+        icon = document.querySelector(`#${language} .dropdown-icon`);
+  //toggle dropdown icon and container
+  if (booksContainer.classList.contains('show')) {
+    booksContainer.classList.remove('show');
+    icon.classList.remove('show');
+  } else {
+    booksContainer.classList.add('show');
+    icon.classList.add('show');
+  }
+}
