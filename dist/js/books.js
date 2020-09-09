9ca795f1fb301fb30f17b1b5e503c6453d63cc3e
iamndlovu <ndlovu.p@protonmail.com>
2020-09-09 20:17:00 +0200

    added links to each book

--- dist/js/books.js
+++ dist/js/books.js
@@ -1,49 +1,49 @@
 //english
-const lostAngel = new Book('lost angel', 'english', 'lost-angel-cover.JPG', lostSummary, [{href: '#', classList: 'fab fa-amazon'}], ['English', 'Drama', 'Horror']);
+const lostAngel = new Book('lost angel', 'english', 'lost-angel-cover.JPG', lostSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['English', 'Drama', 'Horror']);
 
-const myDaughter = new Book('i give you my daughter: don\'t disappoint her!', 'english', 'my-daughter-cover.JPG', daughterSummary, [{href: '#', classList: 'fab fa-amazon'}], ['English', 'Drama', 'Horror']);
+const myDaughter = new Book('i give you my daughter: don\'t disappoint her!', 'english', 'my-daughter-cover.JPG', daughterSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['English', 'Drama', 'Horror']);
 
 
 //ndebele
-const emahlukanandlela = new Book('Emahlukanandlela', 'ndebele', 'emahlukanandlela-cover.JPG', emahlukanandlelaSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama']);
+const emahlukanandlela = new Book('Emahlukanandlela', 'ndebele', 'emahlukanandlela-cover.JPG', emahlukanandlelaSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama']);
 
-const emalangabini = new Book('Emalangabini othando lezinye indatshana', 'ndebele', 'emalangabini-cover.JPG', emalangabiniSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Indatshana']);
+const emalangabini = new Book('Emalangabini othando lezinye indatshana', 'ndebele', 'emalangabini-cover.JPG', emalangabiniSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Indatshana']);
 
-const emanxiweni = new Book('Emanxiweni othando', 'ndebele', 'emanxiweni-cover.JPG', emanxiweniSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Romance']);
+const emanxiweni = new Book('Emanxiweni othando', 'ndebele', 'emanxiweni-cover.JPG', emanxiweniSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'} ,{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Romance']);
 
-const emdlalweni = new Book('emdlalweni wempilo', 'ndebele', 'emdlalweni-cover.JPG', emdlalweniSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Romance']);
+const emdlalweni = new Book('emdlalweni wempilo', 'ndebele', 'emdlalweni-cover.JPG', emdlalweniSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Romance']);
 
-const impilo = new Book('impilo le!', 'ndebele', 'impilo-cover.JPG', impiloSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama']);
+const impilo = new Book('impilo le!', 'ndebele', 'impilo-cover.JPG', impiloSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama']);
 
-const intombi = new Book('intombi yomthandazo', 'ndebele', 'intombi-cover.JPG', intombiSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama', 'Romance']);
+const intombi = new Book('intombi yomthandazo', 'ndebele', 'intombi-cover.JPG', intombiSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama', 'Romance']);
 
-const isikhwili = new Book('isikhwili', 'ndebele', 'isikhwili-cover.JPG', isikhwiliSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama', 'Crime']);
+const isikhwili = new Book('isikhwili', 'ndebele', 'isikhwili-cover.JPG', isikhwiliSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama', 'Crime']);
 
-const izithukuthuku = new Book('izithukuthuku', 'ndebele', 'izithukuthuku-cover.JPG', izithukuthukuSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama', 'adventure', 'action']);
+const izithukuthuku = new Book('izithukuthuku', 'ndebele', 'izithukuthuku-cover.JPG', izithukuthukuSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'Drama', 'adventure', 'action']);
 
 const ukhandalenyoka = new Book('ukhandalenyoka', 'ndebele', 'khandalenyoka-cover.JPG', ukhandalenyokaSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const linda = new Book('linda ungowami', 'ndebele', 'linda-cover.JPG', lindaSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const linda = new Book('linda ungowami', 'ndebele', 'linda-cover.JPG', lindaSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const ngakonelani = new Book('ngakonelani mntakamama?', 'ndebele', 'ngakonelani-cover.JPG', ngakonelaniSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const ngakonelani = new Book('ngakonelani mntakamama?', 'ndebele', 'ngakonelani-cover.JPG', ngakonelaniSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const ngisakhumbula = new Book('ngisakhumbula', 'ndebele', 'ngisakhumbula-cover.JPG', ngisakhumbulaSummary, [{href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const ngisakhumbula = new Book('ngisakhumbula', 'ndebele', 'ngisakhumbula-cover.JPG', ngisakhumbulaSummary, [{href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const owami = new Book('owami uzamthatha kuphela!', 'ndebele', 'owami-cover.JPG', owamiSummary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const owami = new Book('owami uzamthatha kuphela!', 'ndebele', 'owami-cover.JPG', owamiSummary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const selimathunzi = new Book('selimathunzi', 'ndebele', 'selimathunzi-cover.JPG', selimathunziSummary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const selimathunzi = new Book('selimathunzi', 'ndebele', 'selimathunzi-cover.JPG', selimathunziSummary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const umuzi = new Book('umuzi wakhiwa ngezithukuthuku', 'ndebele', 'umuzi-cover.JPG', umuziSummary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const umuzi = new Book('umuzi wakhiwa ngezithukuthuku', 'ndebele', 'umuzi-cover.JPG', umuziSummary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const ungangeni = new Book('ungangeni ngenhliziyo yonke!', 'ndebele', 'ungangeni-cover.JPG', ungangeniSummary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const ungangeni = new Book('ungangeni ngenhliziyo yonke!', 'ndebele', 'ungangeni-cover.JPG', ungangeniSummary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const ungawubasa = new Book('ungawubasa uzawotha', 'ndebele', 'ungawubasa-cover.JPG', ungawubasaSummary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const ungawubasa = new Book('ungawubasa uzawotha', 'ndebele', 'ungawubasa-cover.JPG', ungawubasaSummary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const usakhula = new Book('ngakuthanda usakhula', 'ndebele', 'usakhula-cover.JPG', usakhulaSummary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const usakhula = new Book('ngakuthanda usakhula', 'ndebele', 'usakhula-cover.JPG', usakhulaSummary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const uzangibonga1 = new Book('uzangibonga kusasa 1', 'ndebele', 'uzangibonga-1-cover.JPG', uzangibonga1Summary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const uzangibonga1 = new Book('uzangibonga kusasa 1', 'ndebele', 'uzangibonga-1-cover.JPG', uzangibonga1Summary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
-const uzangibonga2 = new Book('uzangibonga kusasa 2', 'ndebele', 'uzangibonga-2-cover.JPG', uzangibonga2Summary, [{href: '#', classList: 'fa fa-amazon'}], ['Ndebele', 'adventure', 'action']);
+const uzangibonga2 = new Book('uzangibonga kusasa 2', 'ndebele', 'uzangibonga-2-cover.JPG', uzangibonga2Summary, [{href: '#', classList: 'fa fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}, {href: '#', classList: 'fab fa-amazon'}], ['Ndebele', 'adventure', 'action']);
 
 
 
